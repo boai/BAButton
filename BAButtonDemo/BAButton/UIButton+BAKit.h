11f3682f35a29082f0aedefdfcb6a3c23216089c
boai <sunboyan@outlook.com>
2017-05-19 15:22:50 +0800

    新增圆角设置

---
+++ BAButtonDemo/BAButton/UIButton+BAKit.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+BAKit.h
+//  BAButtonDemo
+//
+//  Created by boai on 2017/5/17.
+//  Copyright © 2017年 博爱之家. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (BAKit)
+
+@end
---
+++ BAButtonDemo/BAButton/UIButton+BAKit.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+BAKit.m
+//  BAButtonDemo
+//
+//  Created by boai on 2017/5/17.
+//  Copyright © 2017年 博爱之家. All rights reserved.
+//
+
+#import "UIButton+BAKit.h"
+
+@implementation UIButton (BAKit)
+
+@end
