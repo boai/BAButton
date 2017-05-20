5794251573cf24f03f8fa4be70dc11373fb703da
boai <sunboyan@outlook.com>
2017-05-20 10:52:56 +0800

    最新更新时间：2017-05-20 【倒叙】  最新Version：【Version：2.3.0】  更新内容：  2.3.0.1、用分类全新重构 BAButton，代码配置更灵活  2.3.0.2、可以自由设置 button 的文字和图片的布局、间距、每个角的切圆角  2.3.0.3、全面适配 纯代码、xib、storyboard  2.3.0.4、版本改动较大，希望大家谅解，如果用老版本的 BAButton，可以参考demo 更换新版本，后期改动不大

---
+++ BAButton/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  BAButton
+//
+//  Created by boai on 2017/5/20.
+//  Copyright © 2017年 boai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
---
+++ BAButton/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BAButton
+//
+//  Created by boai on 2017/5/19.
+//  Copyright © 2017年 boai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
